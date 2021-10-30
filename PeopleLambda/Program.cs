2c3cc1303d7acb7bd7445bdcb28ed70bf9653919
Mhnd <mrmohandtv@gmail.com>
2021-10-30 18:11:39 +0300

    Add project files.

---
+++ PeopleLambda/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace PeopleLambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //LambdaTest();
+            LinqTest();
+            Console.WriteLine("Done processing");
+            Console.ReadLine();
+        }
+
+        public static void LinqTest()
+        {
+            var people = SampleData.GetPeopleData();
+
+            var results = (from p in people orderby p.FirstName
+                           where p.LastName.ToLower() == "algamdi" select p);
+
+            foreach (var person in results)
+            {
+                Console.WriteLine($"{ person.FirstName } { person.LastName } { person.Email }");
+            }
+        }
+
+        public static void LambdaTest()
+        {
+            var people = SampleData.GetPeopleData();
+
+            var results = people.Where(p => p.LastName.ToLower() == "algamdi").OrderBy(p => p.FirstName);
+
+            foreach (var person in results)
+            {
+                Console.WriteLine($"{ person.FirstName } { person.LastName } { person.Email }");
+            }
+        }
+    }
+}
---
+++ PeopleLambda/SampleData.cs
@@ -0,0 +1,24 @@
+﻿using PeopleLambda.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleLambda
+{
+    public class SampleData
+    {
+        public static List<PersonModel> GetPeopleData()
+        {
+            List<PersonModel> people = new List<PersonModel>
+            {
+                new PersonModel{FirstName = "Mohanned", LastName = "Akbar", Email = "mrmohandtv@gmail.com"},
+                new PersonModel{FirstName = "Amjad", LastName = "Bukhari", Email = "amj@gmail.com"},
+                new PersonModel{FirstName = "Odi", LastName = "Algamdi", Email = "odi@gmail.com"},
+                new PersonModel{FirstName = "Loay", LastName = "Algamdi", Email = "loay@gmail.com"},
+                new PersonModel{FirstName = "Yehya", LastName = "Asfor", Email = "yehya@gmail.com"}
+            };
+
+            return people;
+        }
+    }
+}
